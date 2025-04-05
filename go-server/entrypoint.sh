f314bd4401d0747aba0d7eb73ea0b4edde1ff370
Sam Bernheim <sambernheim@icloud.com>
2025-04-05 14:42:43 -0400

    chore: working with delve

--- go-server/entrypoint.sh
+++ go-server/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-dlv debug ./main.go --headless --listen=:2345 --api-version=2 --accept-multiclient &
+# dlv debug ./main.go --headless --listen=:2345 --api-version=2 --accept-multiclient&
 
 # Start Air to watch for changes
 air -c .air.toml
